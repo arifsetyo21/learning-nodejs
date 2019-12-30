9e836d373d3cb707970617a500313d54798e728f
arifsetyo21 <arifsetyo19@gmail.com>
2019-12-30 23:44:58 +0700

    first commit

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const http = require('http')
+
+///////////////// NOTE FILE
+
+/* NOTE Syncronous Way */
+/* NOTE Read file in node.js */
+/* const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn);
+ */
+/* NOTE Write file in node.js */
+/* const textOut = `This is what we know about avocado : ${textIn}. \nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/output.txt', textOut)
+console.log('File Written!') */
+
+/* NOTE Non-blocking/ asyncronous way */
+/* fs.readFile('./txt/start.txt', 'utf-8', function (err, data1) {
+   fs.readFile(`./txt/${data1}.txt`, 'utf-8', function (err, data2) {
+      console.log(data2)
+      fs.readFile('./txt/append.txt', 'utf-8', function (err, data3) {
+         console.log(data3)
+         fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+            console.log('Your file has been written!');
+         })
+      })
+   })
+}) 
+console.log('will read file!');
+*/
+
+////////////////// NOTE SERVER
+const server = http.createServer((req, res) => {
+   console.log(req);
+   res.end('Hello from the server!')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+   console.log('Listening to request on port 8000');
+})
