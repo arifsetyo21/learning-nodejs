dc51a273787a21b17401fe25acfc3fbc425ac58f
arifsetyo21 <arifsetyo19@gmail.com>
2019-12-31 14:33:14 +0700

    create template for diplaying overview, product and cards

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -29,10 +29,14 @@ console.log('File Written!') */
 console.log('will read file!');
 */
 
+const tempOveview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
 /* NOTE loading file syncronously for eficiency */
 /* This code will not calling again after first request */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const productData = JSON.parse(data)
+const dataObj = JSON.parse(data)
 
 ////////////////// NOTE SERVER and URL(Routings)
 const server = http.createServer((req, res) => {
@@ -40,13 +44,21 @@ const server = http.createServer((req, res) => {
    console.log(req.url);
 
    /* NOTE Native routing with if clause */
+   /* NOTE Overview Page */
    if (pathName === '/' || pathName === '/overview') {
-      res.end('this is a overview page')
+      res.writeHead(200, {'Content-Type' : 'text/html'})
+      res.end(tempOveview)
+      
+   /* NOTE Product Page */
    } else if ( pathName === '/product') {
       res.end('this is a product page')
+
+   /* NOTE API */
    } else if ( pathName === '/api'){      
       res.writeHead(200, {'Content-Type' : 'application/json'})
       res.end(data)   
+   
+   /* NOTE Not found */
    } else {
       /* NOTE return 404 if the resource not found */
       res.writeHead(404, {
