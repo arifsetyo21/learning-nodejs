3d1e81be787d7434a8db17a2c4d0ee253706aa96
arifsetyo21 <arifsetyo19@gmail.com>
2020-01-02 02:06:10 +0700

    Create resource /api/v1/tours/:id for handling detail tours

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -23,6 +23,31 @@ app.get('/api/v1/tours', function(req, res) {
    });
 });
 
+app.get('/api/v1/tours/:id', function(req, res) {
+   /* NOTE request spesific data with id param */
+   /* NOTE * 1 will convert string like integer to integer data type */
+   const id = req.params.id * 1;
+
+   /* NOTE find object with id object, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find */
+   const tour = tours.find(el => el.id === id);
+
+   // if (id > tours.length) {
+   /* NOTE  if tour not found, its will return undefined */
+   if (!tour) {
+      return res.status(404).json({
+         status: 'fail',
+         message: 'Invalid ID'
+      });
+   }
+
+   res.status(200).json({
+      status: 'success',
+      data: {
+         tour
+      }
+   });
+});
+
 app.post('/api/v1/tours', (req, res) => {
    const newId = tours[tours.length - 1].id + 1;
    const newTour = Object.assign({ id: newId }, req.body);
