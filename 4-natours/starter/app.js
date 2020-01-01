e43b9e6168fbe2888bd0886d03842acf1cad3d2f
arifsetyo21 <arifsetyo19@gmail.com>
2020-01-02 00:20:09 +0700

    Create POST resource for create new tour in /api/v1/tours

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+/* NOTE This is how use middleware in express */
+app.use(express.json());
+
 const port = 8000;
 
 const tours = JSON.parse(
@@ -20,8 +23,24 @@ app.get('/api/v1/tours', function(req, res) {
    });
 });
 
-app.post('/', (req, res) => {
-   res.status(200).send('you can post to this endpoint');
+app.post('/api/v1/tours', (req, res) => {
+   const newId = tours[tours.length - 1].id + 1;
+   const newTour = Object.assign({ id: newId }, req.body);
+
+   tours.push(newTour);
+
+   fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours),
+      err => {
+         res.status(201).json({
+            status: 'success',
+            data: {
+               tour: newTour
+            }
+         });
+      }
+   );
 });
 
 app.listen(port, () => {
