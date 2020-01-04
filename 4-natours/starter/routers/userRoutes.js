7c9c8e54a452c4c7645385ba94c2b230e3f98d7e
arifsetyo21 <arifsetyo19@gmail.com>
2020-01-05 02:28:43 +0700

    Express project structure step 2 : Creating controller structure for user and tour

--- 4-natours/starter/routers/userRoutes.js
+++ 4-natours/starter/routers/userRoutes.js
@@ -1,49 +1,17 @@
 /* NOTE Refactoring 4 route solution, Manage file structure with separate routing to other directory */
 const express = require('express');
-
+const userController = require('../controllers/userController');
 const router = express.Router();
-
-const getAllUsers = (req, res) => {
-   res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-   });
-};
-
-const createUser = (req, res) => {
-   res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-   });
-};
-const getUser = (req, res) => {
-   res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-   });
-};
-const updateUser = (req, res) => {
-   res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-   });
-};
-const deteleUser = (req, res) => {
-   res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-   });
-};
 
 router
    .route('/')
-   .get(getAllUsers)
-   .post(createUser);
+   .get(userController.getAllUsers)
+   .post(userController.createUser);
 
 router
    .route('/:id')
-   .get(getUser)
-   .patch(updateUser)
-   .delete(deteleUser);
+   .get(userController.getUser)
+   .patch(userController.updateUser)
+   .delete(userController.deteleUser);
 
 module.exports = router;
