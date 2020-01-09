6b0639543638cdb9e4347ebcebdfe039fb9ed953
arifsetyo21 <arifsetyo19@gmail.com>
2020-01-09 14:01:50 +0700

    Create new mongoose schema and create mongoose model and save to the database

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -21,6 +21,38 @@ mongoose
       console.log('db connection successfuly');
    });
 
+/* NOTE Create schema creating model  */
+const tourSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: [true, 'a tour must have a name'], //Validation
+      unique: true
+   },
+   rating: {
+      type: Number,
+      default: 4.5
+   },
+   price: {
+      type: Number,
+      required: [true, 'a tour must have a price']
+   }
+});
+
+/* NOTE Creating model for  */
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+   name: 'The Park Camper',
+   price: 997
+});
+
+testTour
+   .save()
+   .then(doc => {
+      console.log(doc);
+   })
+   .catch(err => console.log('======ERROR======', err));
+
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
