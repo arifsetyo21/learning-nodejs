cc70d62455202053918216757da78a99b77d14ff
arifsetyo21 <arifsetyo19@gmail.com>
2020-01-14 02:04:39 +0700

    add new user model create to authcontroller for sign up purpose

--- 4-natours/starter/routers/routes/userRoutes.js
+++ 4-natours/starter/routers/routes/userRoutes.js
@@ -1,8 +1,12 @@
 /* NOTE Refactoring 4 route solution, Manage file structure with separate routing to other directory */
 const express = require('express');
 const userController = require('../../controllers/userController');
+const authController = require('../../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
+
 router
    .route('/')
    .get(userController.getAllUsers)
