dd4fae8f54b64d17808043ecd479d59a057eed1b
arifsetyo21 <arifsetyo19@gmail.com>
2020-01-18 02:37:26 +0700

    update EMAIL_PORT in .env and handling error in utils/email.js

--- 4-natours/starter/utils/email.js
+++ 4-natours/starter/utils/email.js
@@ -1,27 +1,31 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
-   // 1. Create a transpoter
-   const transpoter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-         user: process.env.EMAIL_USERNAME,
-         password: process.env.EMAIL_PASSWORD
-      }
-   });
+   try {
+      // 1. Create a transpoter
+      const transpoter = nodemailer.createTransport({
+         host: process.env.EMAIL_HOST,
+         port: process.env.EMAIL_PORT,
+         auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+         }
+      });
 
-   // 2. Define email option
-   const mailOptions = {
-      from: 'Arif Setyo Nugroho <arif@arif.io>',
-      to: options.email,
-      subject: options.subject,
-      text: options.message
-      // html
-   };
+      // 2. Define email option
+      const mailOptions = {
+         from: 'Arif Setyo Nugroho <arif@arif.io>',
+         to: options.email,
+         subject: options.subject,
+         text: options.message
+         // html
+      };
 
-   // 3. Actually send mail
-   await transpoter.sendMail(mailOptions);
+      // 3. Actually send mail
+      await transpoter.sendMail(mailOptions);
+   } catch (error) {
+      console.log(error);
+   }
 };
 
 module.exports = sendEmail;
