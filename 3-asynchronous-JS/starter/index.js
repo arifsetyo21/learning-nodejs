e9221929fc3fcd86a6f2ad6d77100d26ac0f6532
arifsetyo21 <arifsetyo19@gmail.com>
2020-02-05 02:20:11 +0700

    using method then and catch for handling success and error response

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,18 +1,25 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+// NOTE Read file asyncronously in js
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed : ${data}`);
 
+  //NOTE Using superagent for request to resouce
   superagent
-    .get(`https://dog.ceo/api/breed/shiba/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
+    // NOTE Method which returned a promises
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    // NOTE Handle successfuly case while request using promise
+    .then(res => {
       console.log(res.body.message);
 
+      // NOTE write result path image to file
       fs.writeFile("dog-img.txt", res.body.message, err => {
         console.log("Random dog image saved to file!");
       });
+    })
+    // NOTE Catch for handle rejected or unsuccessfuly image
+    .catch(err => {
+      console.log(err.message);
     });
 });
