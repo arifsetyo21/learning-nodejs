0d8c1b536b3f2d7f1904754a1ffd83e0bde7f29c
arifsetyo21 <arifsetyo19@gmail.com>
2020-02-11 22:31:30 +0700

    create some global error handling middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -94,9 +94,25 @@ app.use((req, res, next) => {
 app.use('/api/v1', indexRouter);
 
 app.all('*', (req, res, next) => {
-   return res.status(404).json({
-      status: 'fail',
-      message: `Can't find ${req.originalUrl} on this server!`
+   // return res.status(404).json({
+   //    status: 'fail',
+   //    message: `Can't find ${req.originalUrl} on this server!`
+   // });
+
+   const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+   err.status = 'fail';
+   err.statusCode = 404;
+
+   next(err);
+});
+
+app.use((err, req, res, next) => {
+   err.statusCode = err.statusCode || 500;
+   err.status = err.status || 'error';
+
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
    });
 });
 
