9ec7cb9fb93d9bb2c4135c79db7b8f4fdad26a70
arifsetyo21 <arifsetyo19@gmail.com>
2020-02-13 23:33:58 +0700

    create errorController, create appError class, refactor app.js and using newest method and class

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,6 +8,8 @@ const hpp = require('hpp');
 
 const dotenv = require('dotenv');
 const indexRouter = require('./routers/index');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 dotenv.config({ path: './.env' });
 const app = express();
@@ -94,27 +96,10 @@ app.use((req, res, next) => {
 app.use('/api/v1', indexRouter);
 
 app.all('*', (req, res, next) => {
-   // return res.status(404).json({
-   //    status: 'fail',
-   //    message: `Can't find ${req.originalUrl} on this server!`
-   // });
-
-   const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-   err.status = 'fail';
-   err.statusCode = 404;
-
-   next(err);
+   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-   err.statusCode = err.statusCode || 500;
-   err.status = err.status || 'error';
-
-   res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-   });
-});
+app.use(globalErrorHandler);
 
 /* NOTE Refactoring route solution 3 with route mounting middleware/group routing if in laravel */
 
