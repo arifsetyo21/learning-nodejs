f92097f5b9bb25008d305ad8e645598a3af6564b
arifsetyo21 <arifsetyo19@gmail.com>
2020-02-14 23:31:36 +0700

    refactoring code to use catch error module separately

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,6 +2,7 @@ const sharp = require('sharp');
 const multer = require('multer');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
 const multerStorage = multer.memoryStorage();
 
@@ -68,96 +69,75 @@ exports.aliasTopTours = async (req, res, next) => {
    next();
 };
 
-exports.getAllTours = async function(req, res) {
-   try {
-      // BUILD QUERY
-      const features = new APIFeatures(Tour.find(), req.query)
-         .filter()
-         .sort()
-         .limit()
-         .paginate();
+exports.getAllTours = catchAsync(async function(req, res, next) {
+   // BUILD QUERY
+   const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .paginate();
 
-      // EXECUTE QUERY
-      const tours = await features.query;
+   // EXECUTE QUERY
+   const tours = await features.query;
 
-      // SEND RESPONSE
-      /* NOTE data : tours is shorthand type if we has same variable name between key pair */
-      res.status(200).json({
-         status: 'success',
-         result: tours.length,
-         data: {
-            tours
-         }
-      });
-   } catch (error) {
-      res.status(400).json({
-         status: 'fail',
-         message: error
-      });
-   }
-};
+   // SEND RESPONSE
+   /* NOTE data : tours is shorthand type if we has same variable name between key pair */
+   res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+         tours
+      }
+   });
+});
 
-exports.getTour = async function(req, res) {
+exports.getTour = catchAsync(async function(req, res, next) {
    console.log(req.params);
-   try {
-      const tour = await Tour.findById(req.params.id);
-      /* Tour.findOne({_id: req.params.id}) */
+   const tour = await Tour.findById(req.params.id);
+   /* Tour.findOne({_id: req.params.id}) */
 
-      res.status(200).json({
-         status: 'success',
-         data: {
-            tour
-         }
-      });
-   } catch (error) {
-      res.status(404).json({
-         status: 'fail',
-         message: 'not found!'
-      });
-   }
-};
+   res.status(200).json({
+      status: 'success',
+      data: {
+         tour
+      }
+   });
+});
 
-exports.updateTour = async (req, res) => {
-   try {
-      const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-         new: true,
-         runValidators: true
-      });
-      /* NOTE 201 mean created */
-      return res.status(201).json({
-         status: 'success',
-         data: {
-            tour
-         }
-      });
-   } catch (error) {
-      res.status(404).json({
-         status: 'fail',
-         message: 'not found!'
-      });
-   }
-};
+exports.updateTour = catchAsync(async (req, res, next) => {
+   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+   });
+   /* NOTE 201 mean created */
+   return res.status(201).json({
+      status: 'success',
+      data: {
+         tour
+      }
+   });
+});
 
-exports.deleteTour = async (req, res) => {
-   try {
-      await Tour.findByIdAndDelete(req.params.id);
-      /* NOTE 204 mean No Content => There is no content to send for this request */
-      return res.status(204).json({
-         status: 'success',
-         data: null
-      });
-   } catch (error) {
-      res.status(404).json({
-         status: 'fail',
-         message: 'not found!',
-         error
-      });
-   }
-};
+exports.deleteTour = catchAsync(async (req, res, next) => {
+   await Tour.findByIdAndDelete(req.params.id);
+   /* NOTE 204 mean No Content => There is no content to send for this request */
+   return res.status(204).json({
+      status: 'success',
+      data: null
+   });
+});
 
-exports.createTour = async (req, res) => {
-   try {
-      /* NOTE Old way to create new tour
+exports.createTour = catchAsync(async (req, res, next) => {
+   /* NOTE New way to create new tour */
+   const newTour = await Tour.create(req.body);
+
+   res.status(201).json({
+      status: 'success',
+      data: {
+         tour: newTour
+      }
+   });
+   /* NOTE Old way to create new tour
       const newTour = new Tour({
          name: 'Bali Paradise',
          price: 305
@@ -165,22 +145,15 @@ exports.createTour = async (req, res) => {
    
       newTour.save(); */
 
-      /* NOTE New way to create new tour */
-      const newTour = await Tour.create(req.body);
+   // try {
 
-      res.status(201).json({
-         status: 'success',
-         data: {
-            tour: newTour
-         }
-      });
-   } catch (err) {
-      res.status(400).json({
-         status: 'fail',
-         message: err
-      });
-   }
-};
+   // } catch (err) {
+   //    res.status(400).json({
+   //       status: 'fail',
+   //       message: err
+   //    });
+   // }
+});
 
 exports.getTourStats = async (req, res) => {
    try {
